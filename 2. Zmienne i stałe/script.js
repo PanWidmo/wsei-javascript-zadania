0bb4baa7b90ad2b790109e2380dea74967c7bff7
PanWidmo <62946233+PanWidmo@users.noreply.github.com>
2020-10-06 10:58:07 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,25 @@
+//Task 1
+var number=2;
+var string="JS";
+var numbStr=2+" XY";
+var logic=3>2;
+var special=null;
+
+console.log(number);
+console.log(string);
+console.log(numbStr);
+console.log(logic);
+console.log(special);
+
+//Task 2
+var number1=2;
+var number2=4;
+
+var sum=0;
+
+sum=number1+number2;
+
+console.log(sum);
+
+//Task 3
+Varaible doesn't have value.
