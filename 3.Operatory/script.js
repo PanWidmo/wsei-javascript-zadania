74f88dabdd873f1107aab7cda40b8b1d897e7789
PanWidmo <62946233+PanWidmo@users.noreply.github.com>
2020-10-06 11:33:27 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,50 @@
+//Task 1
+var bool1=true;
+var bool2=false;
+
+console.log(bool1 === bool2);
+
+//Task 2
+var num1=2;
+var num2=5;
+var moduloResult=0;
+
+moduloResult=num1%num2;
+
+console.log(moduloResult);
+
+//Task 3
+var str1="T";
+var str2="B";
+var stringResault= "";
+
+stringResault=str1+str2;
+
+console.log(stringResault);
+
+//Task 4
+var someNumber=425;
+var someString="425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+== checks values,
+=== checks values and types.
+
+//Task 5
+var counter=30;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//Task 6
+var x=2;
+var y=9;
+var resault=null;
+
+resault=x<y;
+
+console.log(resault);
