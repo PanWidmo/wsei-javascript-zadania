5f1e77ad2254bdff7f3bb0fd1b1a036450e85c2f
PanWidmo <62946233+PanWidmo@users.noreply.github.com>
2020-10-06 10:27:22 +0200

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//Task 1
+console.log("Twoje imię");
+
+//Task 2
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//Task 3
+1. String- it displays text.
+2. Variable without declaration (error).
+3. Missing " sign at the end (error).
