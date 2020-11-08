6a860a74409692bbe26a23617d1a150ebf10fb8e
PanWidmo <62946233+PanWidmo@users.noreply.github.com>
2020-11-08 11:39:08 +0100

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -92,3 +92,30 @@ for(j=0;j<paramTable.length;j++){
 myTable([1,2,3,4]);
 
 //Task 6
+myTable([1,2,3,4]);
+
+function myTable(paramTable){
+    var sum =0;
+    var length = 0;
+    for (i=0; i<paramTable.length; i++){
+        if(paramTable[i] % 2 == 0){
+            
+            sum = sum + paramTable[i];
+            length++;
+        }
+            
+    }
+    let avg = sum / length;
+    console.log(avg)
+};
+
+
+//Task 10
+function reserveSing(paramTable)
+{
+  return  paramTable.map(el => {
+    return el * (-1);
+  })
+}
+console.log(reserveSing([1,2,3,4,6,-6]));
+
