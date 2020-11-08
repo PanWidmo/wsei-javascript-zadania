9683efd5ef872ac7bd59084ae7bd6ccac90882e9
PanWidmo <62946233+PanWidmo@users.noreply.github.com>
2020-11-08 11:58:30 +0100

    Update script.js

---
+++ 8.Obiekty/script.js
@@ -0,0 +1,78 @@
+//Task 1
+var car={
+    name:"BMW",
+    year:2000
+}
+
+console.log(car.name);
+console.log(car.year);
+
+//Task 2
+var car={
+    name:"BMW",
+    year:2000,
+    changeName:function(newName){
+        this.name=newName;
+    }
+}
+
+car.changeName("polonez");
+
+console.log(car.name);
+console.log(car.year);
+
+
+//Task 3
+var newObject = {
+    sum: 0,
+    sumValues: function(array) {
+        array.forEach(item => this.sum += item);
+    }
+}
+
+newObject.sumValues([1,5,7]);
+console.log(newObject.sum)
+
+//Task 4
+var car={
+    name:"BMW",
+    age: 12
+}
+
+
+for(let key in car){
+    console.log(key + ":"+ car[key])
+    
+}
+
+//lub
+//for(let key in car){
+//    console.log(`${key}: ${car[key]}`);
+//}
+
+//Task 5
+const car = {
+	name: 'Fiat',
+	age: 35,
+	color: 'red'
+};
+const carCountryCity = {
+	country: 'Poland',
+	city: 'Krakow'
+};
+car.production = carCountryCity;
+console.log(car.production.country, car.production.city);
+
+
+//Task 6
+var car={
+    name:"BMW",
+    age: 12
+}
+car.width=156;
+
+car.hello=()=>{
+    console.log("Hello");
+}
+
+car.hello();
