ec916aa031dc6d2e411007211b66e9861f0e7179
PanWidmo <62946233+PanWidmo@users.noreply.github.com>
2020-11-22 10:12:00 +0100

    Update script.js

--- 9.Konstruktory i prototype/script.js
+++ 9.Konstruktory i prototype/script.js
@@ -1,16 +1,28 @@
 //Task 1
-function Person(firstname,lastname,age,country,city,language){
-    this.firstname=firstname,
-    this.lastname=lastname,
-    this.age=age,
-    this.country=country,
-    this.city=city,
-    this.language=language
+function Person(name, lastName, age, country, city, language) {
+	this.name = name;
+	this.lastname = lastName;
+	this.age = age;
+	this.country = country;
+	this.city = city;
+	this.language = language;
+	this.changeCity = function (newCity) {
+		return (this.city = newCity);
+	};
+	this.changeAge = function (newAge) {
+		return (this.age = newAge);
+	};
 }
+const p1 = new Person('marek', 'kowalski', 25, 'poland', 'lublin', 'pl');
+const p2 = new Person('roman', 'nowak', 37, 'poland', 'krakow', 'pl');
+const p3 = new Person('francis', 'kawa', 40, 'france', 'paris', 'fr');
+const p4 = new Person('jakub', 'kot', 44, 'poland', 'katowice', 'pl');
+const p5 = new Person('adolf', 'hool', 28, 'germany', 'berlin', 'de');
 
-var user1 =new Person("Jan","Dzban",22,"Polska","Krakow","polski");
+p1.changeAge(33);
+p1.changeCity('radom');
 
-console.log(user1);
+console.log(p1, p2, p3, p4, p5);
 
 //Task 2
 function Calculator(){
@@ -48,3 +60,23 @@ calc1.dziel(10,5)
 calc1.showMemory()
 
 //Task 3
+    function Game() {
+	this.changeNumber = function () {
+		Game.prototype.interval = setInterval(() => {
+			Game.prototype.number = Math.round(Math.random() * 10);
+			console.log(Game.prototype.number);
+		}, 1000);
+	};
+	this.checkNumber = function () {
+		Game.prototype.check = setInterval(() => {
+			if (Game.prototype.number > 5) {
+				clearInterval(Game.prototype.interval);
+				clearInterval(Game.prototype.check);
+			}
+		}, 1000);
+	};
+}
+const game1 = new Game();
+const game2 = new Game();
+game1.changeNumber();
+game2.checkNumber();
